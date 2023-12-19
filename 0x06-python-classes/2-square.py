fd4361ed79d645fd729b2c4b9ef9a14d9b393f41
Jim-03 <jcnyongesa@kabarak.ac.ke>
2023-12-19 12:22:18 +0300

    defines a square but with instantiation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""module for a square"""
+
+
+class Square:
+    """defines a square"""
+    def __init__(self, size=0):
+        """initialize.
+        Args:
+            size: the length of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        elif size < 0:
+            raise ValueError('size must be >= 0')
+        self.__size = size
