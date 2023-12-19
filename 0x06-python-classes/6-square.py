480ad38de7ce975a0e1de53e79cec3ec91d30ee6
Jim-03 <jcnyongesa@kabarak.ac.ke>
2023-12-19 17:57:35 +0300

    sets the position

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -5,12 +5,14 @@
 class Square:
     """defines a square"""
 
-    def __init__(self, size=0):
+    def __init__(self, size=0, position=(0, 0)):
         """initializer
         Args:
             size (int): the length of the square
+            postion (int, int): position of the square
         """
         self.size = size
+        self.position = position
 
     @property
     def size(self):
@@ -24,6 +26,20 @@ class Square:
         elif val < 0:
             raise ValueError('size must be >= 0')
         self.__size = val
+
+    @property
+    def position(self):
+        """the current position of the square"""
+        return (self.__position)
+
+    @position.setter
+    def position(self, val):
+        if (not isinstance(val, tuple) or
+                len(val) != 2 or
+                not all(isinstance(num, int) for num in val) or
+                not all(num >= 0 for num in val)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = val
 
     def area(self):
         """finds the area of the square"""
