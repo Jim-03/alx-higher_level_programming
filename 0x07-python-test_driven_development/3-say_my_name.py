f9aef9d236cb3e43aaf6e9c73c4c7e8dcec88da3
Jim-03 <jcnyongesa@kabarak.ac.ke>
2024-01-03 17:51:14 +0300

    prints a name

---
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""prints a name"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Args:
+        first_name: the first name
+        last_name: the last name
+    Raise:
+        TypeError: when the arguments aren't strings
+    """
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    elif not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+    print(f"My name is {first_name} {last_name}")
+
+    if __name__ == "__main__":
+        import doctest
+        doctest.testfile("tests/3-say_my_name.txt")
