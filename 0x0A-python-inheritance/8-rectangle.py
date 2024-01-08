21fae20e91030fc98a6481e9ad45fe7f932b954b
Jim-03 <jcnyongesa@kabarak.ac.ke>
2024-01-08 18:22:06 +0300

    An inheritor

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Module for inheritance"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+class Rectangle(BaseGeometry):
+    """Represents a rectangle."""
+    def __init__(self, width, height):
+        """ A new Rectangle.
+        Args:
+            width: the width of the rectangle
+            height: the height of the rectangle
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
