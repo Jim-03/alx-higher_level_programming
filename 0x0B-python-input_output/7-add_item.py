67738ff0a19603831a59565be41df73841f35b92
Jim-03 <jcnyongesa@kabarak.ac.ke>
2024-01-09 18:53:41 +0300

    script that adds all arguments to a Python list

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module to add all arguments to a Python list, and save them to a file."""
+import sys
+
+
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        pythonList = load_from_json_file('add_item.json')
+    except FileNotFoundError:
+        pythonList = []
+    pythonList.extend(sys.argv[1:])
+    save_to_json_file(pythonList, 'add_item.json')
