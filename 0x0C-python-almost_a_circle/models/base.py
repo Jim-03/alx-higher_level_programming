007cd99fc01dc20383df8c45904172b7a43939b2
Jim-03 <jcnyongesa@kabarak.ac.ke>
2024-01-11 13:16:15 +0300

    The base class

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""defines a base class"""
+
+
+class Base:
+    """The base class."""
+    __nb_objects = 0
+    def __init__(self, id=None):
+        """Initializes the base class."""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
