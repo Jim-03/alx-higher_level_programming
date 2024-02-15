2793ab14befb3cb1223f49cc19497cafb4d9e3b5
Jim-03 <jcnyongesa@kabarak.ac.ke>
2024-02-15 20:04:43 +0300

    creates a db with table having relationship

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- creates a db and table
-CREATE DATABASE IF NOT EXIST hbtn_0d_usa;
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE states(
+CREATE TABLE IF NOT EXISTS states(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(256) NOT NULL
 );
