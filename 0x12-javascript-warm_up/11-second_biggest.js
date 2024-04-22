4355788e4901e6d96de543e18957eec298f3c4b4
Jim <jimmycn03@gmail.com>
2024-04-22 18:05:10 +0300

    updating a value in an object

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,6 +4,7 @@ if (args[0] === undefined || args.length === 1) {
   console.log(0);
 } else {
   let max = parseInt(args[0], 10);
+  let max2 = parseInt(args[0], 10);
   for (let i = 1; i < args.length; i++) {
     const value = parseInt(args[i], 10);
     if (value > max) {
@@ -11,5 +12,11 @@ if (args[0] === undefined || args.length === 1) {
     }
   }
 
-  console.log(max);
+  for (let i = 1; i < args.length; i++) {
+    const value = parseInt(args[i], 10);
+    if (value === max) {
+      continue;
+    } else if (max2 < value) { max2 = value; }
+  }
+  console.log(max2);
 }
