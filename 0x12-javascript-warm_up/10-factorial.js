27f86649fe25dd23af06c2719d59d87b3cf020b3
Jim <jimmycn03@gmail.com>
2024-04-22 17:37:36 +0300

    Recursivley finds the factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+function fact(x) {
+  if (x <= 1) {
+    return 1;
+  }
+  let result = x;
+  return result * fact(x - 1);
+}
+
+const args = process.argv.slice(2);
+const x = parseInt(args[0], 10);
+if (isNaN(x)) {
+  console.log(1);
+} else {
+  console.log(fact(x));
+}
