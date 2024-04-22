a664eae22e63adc57ea99fd97d4370c8be7a7233
Jim <jimmycn03@gmail.com>
2024-04-22 17:10:13 +0300

    using a function to add two integers

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+function add (a, b) {
+  return a + b;
+}
+
+const args = process.argv.slice(2);
+const a = parseInt(args[0], 10);
+const b = parseInt(args[1], 10);
+if (isNaN(a) || isNaN(b)) {
+  console.log('NaN');
+} else {
+  console.log(add(a, b));
+}
