ac5ad604b6c43fd3861c11a8176d7dcfe3866d9a
Jim <jimmycn03@gmail.com>
2024-04-23 11:57:42 +0300

    corrected JavaScript spelling

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+const array = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let line = 0; line < array.length; line++) {
   console.log(array[line]);
 }
