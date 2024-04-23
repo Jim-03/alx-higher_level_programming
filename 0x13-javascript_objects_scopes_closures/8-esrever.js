ea27d6a81a2e763a354f726aa278b047d04af0de
Jim <jimmycn03@gmail.com>
2024-04-23 10:21:16 +0300

    Reverses a list

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  for (let element = 0; element < Math.floor(list.length / 2); element++) {
+    const newVal = list[element];
+    list[element] = list[list.length - 1 - element];
+    list[list.length - 1 - element] = newVal;
+  }
+  return list;
+}
