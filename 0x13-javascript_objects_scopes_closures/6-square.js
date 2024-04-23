c6616f85b5a28c3883d8907281592edc06b5f870
Jim <jimmycn03@gmail.com>
2024-04-23 10:01:51 +0300

    inherits a square from 5-square and added charPrint() to print it in a given character

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+const square = require('./5-square.js')
+
+
+class Square extends square {
+  charPrint(c) {
+    let char = '';
+    if (c === undefined) {
+      char = 'X';
+    }
+    else {
+        char = c;
+    }  
+    for (let down = 0; down < this.width; down++){
+      let row = '';
+      for (let side = 0; side < this.width; side++) {
+        row += char;
+      }
+      console.log(row);
+    }
+  }
+}
+
+module.exports = Square;
+
