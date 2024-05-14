7d80b8d87f7db384ef2932893ad694852d1715c5
Jim <jimmycn03@gmail.com>
2024-05-14 15:03:50 +0300

    Prints the bytes of response body

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/bash
+
+#Takes in a URL then displays the body size in bytes
+response=$(curl -s -o /tmp/response.txt -w "%{size_download}" "$1")
+
+echo "$response"
+
+rm /tmp/response.txt >/dev/null 2>&1
