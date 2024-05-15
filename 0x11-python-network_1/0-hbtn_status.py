05fc1eee876f3e2be8320827501948e59bd524b5
Jim <jimmycn03@gmail.com>
2024-05-15 11:14:39 +0300

    Corrected and commented the code

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,11 +3,12 @@ import urllib.request
 """ Fetches from an internet resource."""
 
 
-url = 'https://alx-intranet.hbtn.io/status'i  # The url
+url = 'https://alx-intranet.hbtn.io/status'  # The url
 req = urllib.request.Request(url)  # Request
+# Make the request and close connection when done
 with urllib.request.urlopen(req) as response:
     body = response.read()
-
+# Display the content
 print("Body response:")
 print("\t- type:", type(body))
 print("\t- content:", body)
