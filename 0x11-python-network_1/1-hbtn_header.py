f308de4f7f95097241424ffabe7c095254955bf2
Jim <jimmycn03@gmail.com>
2024-05-15 12:32:59 +0300

    Made the file non executable when imported

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 """ Module takes in a url,
-sends a request
-and displays id from the header.
+sends a request,
+displays id from the header.
 """
 import sys
 import urllib.request
 
 
-url = sys.argv[1]  # The url argument
-req = urllib.request.Request(url)  # Request Handler
-# Make the request with resource closing at the end
-with urllib.request.urlopen(req) as response:
-    x_id = response.getheader('X-Request-Id')  # The reponse id
-print(x_id)  # Print the id
+if __name__ == "__main__":
+    url = sys.argv[1]  # The url argument
+    req = urllib.request.Request(url)  # Request Handler
+    # Make the request with resource closing at the end
+    with urllib.request.urlopen(req) as response:
+        x_id = response.getheader('X-Request-Id')  # The reponse id
+    print(x_id)  # Print the id
--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,19 +3,23 @@
 sends post request
 and displays the body.
 """
+
+
 import urllib.request
 import urllib.parse
 import sys
 
 
-value ={'email': sys.argv[2]}  # Variable for post request
-# Encode the value in a standard format
-data = urllib.parse.urlencode(value)
-data = data.encode('ascii')  # Data in bytes
-# Handle a request
-req = urllib.request.Request(sys.argv[1])  # Parameter contains url from command line
-# Connect and disconnect after making a request
-with urllib.request.openurl(req) as response:
-    # Read the body
-    result = response.read()
-print(result.decode('utf-8'))
+if __name__ == "__main__":
+    value = {'email': sys.argv[2]}  # Variable for post request
+    # Encode the value in a standard format
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')  # Data in bytes
+    # Handle a request
+    # Parameter contains url from command line
+    req = urllib.request.Request(sys.argv[1])
+    # Connect and disconnect after making a request
+    with urllib.request.openurl(req) as response:
+        # Read the body
+        result = response.read()
+    print(result.decode('utf-8'))
