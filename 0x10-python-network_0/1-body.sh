96574dcd9def4efb319880b38c61b12d155dddd3
Jim <jimmycn03@gmail.com>
2024-05-15 06:21:03 +0300

    Displays body of a 200 status code response

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,20 +1,3 @@
 #!/bin/bash
-
 #Takes in a URL and sends a get request and displays response body
-
-if ! [[ "$1" =~ ^https?:// ]]; then
-    url="http://$1"
-else
-    url="$1"
-fi
-
-response=$(curl -s -w "%{http_code}" -o /tmp/response.txt "$1")
-
-status_code=$(tail -c 3 /tmp/response.txt)
-
-if [ "$status_code" = "200" ];
-then
-	cat /tmp/response.txt
-fi
-
-rm /tmp/response.txt >/dev/null 2>&1
+curl -s "$1" | grep -Eo '(^HTTP.*\n)*' 
