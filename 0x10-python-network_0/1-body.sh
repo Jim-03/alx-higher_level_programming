9567d62832038bfeb92b98ce7e689394fa6854a4
Jim <jimmycn03@gmail.com>
2024-05-15 06:25:49 +0300

    Added a filter method

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Takes in a URL and sends a get request and displays response body
-curl -s "$1" | grep -Eo '(^HTTP.*\n)*' 
+status_code=$(curl -s -w "%{http_code}" -o /dev/null "$1"); [ "$status_code" = "200" ] && curl -s "$1" 
