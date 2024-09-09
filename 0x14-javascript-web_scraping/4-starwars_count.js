0fe412315dc4ffff31405556f3e229fca90543e0
Jim <jimmycn03@gmail.com>
2024-09-09 12:59:52 +0300

    Lists the number of movies a character is in

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+const characterId = '18';
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.log(error);
+  } else {
+    const data = JSON.parse(body);
+
+    let count = 0;
+    data.results.forEach(film => {
+      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+        count++;
+      }
+    });
+    console.log(count);
+  }
+});
